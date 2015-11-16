9026c1831582689cd1c82457fbbd66c50b805b27
Sascha Weidner <support@sioweb.de>
2015-11-16 09:33:25 +0100

    Einträge, Buttons, etc.

---
+++ plugins/facebook/fragments/buttons.php
@@ -0,0 +1,10 @@
+<div class="rex-panel-options">
+  <div class="btn-group btn-group-xs">
+    <a href="#slice57" class="btn btn-visible" title="Deaktivieren">
+      <i class="rex-icon rex-icon-visible"></i>
+    </a>
+    <a href="#slice57" class="btn btn-highlight" title="Hervorheben">
+      <i class="rex-icon rex-icon-highlight"></i>
+    </a>
+  </div>
+</div>
--- plugins/facebook/pages/entries.php
+++ plugins/facebook/pages/entries.php
@@ -1,5 +1,28 @@
 <?php
 
+$sections = '';
 $sh = rex_socialhub_facebook::factory();
-foreach($sh->entries() as $key => $value)
-  echo '<pre>'.print_r($value,1).'</pre><br><br>';
+foreach($sh->entries() as $key => $value) {
+  $fragment = new rex_fragment();
+  $fragment->setVar('values',$value);
+  $entry = $fragment->parse('entry.php');
+
+  $buttons = new rex_fragment();
+  $buttons = $fragment->parse('buttons.php');
+
+  $fragment = new rex_fragment();
+  $fragment->setVar('class', 'info', false);
+  $fragment->setVar('title', 'Seite: <a href="https://www.facebook.com/'.$value['author']['id'].'" target="_blank" title="Facebook-Profil '.$value['name'].' aufrufen">'.$value['name'].'</a>',false);
+  $fragment->setVar('heading',$buttons,false);
+  $fragment->setVar('body', $entry, false);
+
+  $likers = [];
+  foreach($value['likes']['data'] as $key => $liker)
+    $likers[] = '<a href="https://www.facebook.com/'.$liker['id'].'" target="_blank">'.$liker['name'].'</a>';
+
+  $fragment->setVar('footer','<p>Likes: '.$value['count_likes'].($value['count_likes'] > 0?' ('.implode(', ',$likers).')':'').'</p>', false);
+
+  $sections .= $fragment->parse('core/page/section.php');
+}
+
+echo $sections;
