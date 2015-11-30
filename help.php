0e4ffed003cc845afe0cb4b7a6e37c95b185779a
Sascha Weidner <support@sioweb.de>
2015-11-30 15:45:14 +0100

    Help, Readme

--- help.php
+++ help.php
@@ -1,1 +1,1 @@
-<p>Readme, Hilfe, Fehlermelden: <a href="https://github.com/factorylabs/redaxo_socialhub" target="_blank">https://github.com/factorylabs/redaxo_socialhub</a></p>
+<p>Readme, Hilfe, Fehlermelden: <a href="https://github.com/factorylabs/redaxo_socialhub" target="_blank">Addon auf Github</a></p>
