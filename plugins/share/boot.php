0b541e15073352f6c6548856ae05962cf54ec0f2
Sascha Weidner <support@sioweb.de>
2015-12-09 14:54:58 +0100

    .share

--- plugins/share/boot.php
+++ plugins/share/boot.php
@@ -1,15 +1,17 @@
 <?php
+echo $_GET['share'];
+if(($short = rex_request('share','string')) !== '') {
+  $Plattform = rex_sql::factory()
+    ->setTable(rex::getTablePrefix().'socialhub_share')
+    ->setWhere(['short'=>$short])
+    ->select()
+    ->getArray();
 
-// if(($short = rex_request('share','string')) !== '') {
-//   $Plattform = rex_sql::factory()
-//     ->setTable(rex::getTablePrefix().'socialhub_share')
-//     ->setWhere(['short'=>$short])
-//     ->select()
-//     ->getArray();
-
-//   echo '<pre>'.print_r($Plattform,1).'</pre>';
-//   die();
-// }
+  if(!empty($Plattform)) {
+    header("Location: ".$Plattform[0]['link'].'?'.str_replace('&amp;','&',preg_replace('|share=[^&]+&rex_modal=1&|is','',$_SERVER['QUERY_STRING'])));
+    exit();
+  }
+}
 
 if(!rex::isBackend())
   rex_extension::register('SLICE_OUTPUT','socialhub_share::add_buttons');
--- plugins/share/fragments/share.php
+++ plugins/share/fragments/share.php
@@ -1,6 +1,6 @@
 <?php $buttons = $this->getVar('buttons');?><div class="socialhub_share">
   <?php foreach($buttons as $key => $button) {?>
-  <a class="shore-icon openModal" href="index.php?share=<?php echo $button['short'];?>&amp;rex_modal=1&amp;<?php echo $button['query'];?>" title="<?php echo $button['title'];?>">
+  <a class="shore-icon openModal" onclick="open('index.php?share=<?php echo urlencode($button['short']);?>&amp;rex_modal=1&amp;<?php echo $button['query'];?>','<?php echo $button['title'];?>','width=500 height=300');return false;" href="index.php?share=<?php echo $button['short'];?>&amp;rex_modal=1&amp;<?php echo $button['query'];?>" title="<?php echo $button['title'];?>">
     <?php if(strpos($button['icon'],0,3) === 'fa-' && $this->getVar('fontawesome') != false) {?><i class="rex-icon <?php echo $button['icon'];?>"></i><?php } else {?>
     <span class="share-short"><?php echo $button[($this->getVar('short')?'short':'title')];?></span><?php }?></a>
   <?php if(($delimitter = $this->getVar('delimitter')) && $button !== end($buttons)) {?><span class="share_delimitter"><?php echo $delimitter;?></span><?php }?>
