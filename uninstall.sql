ffd2052696996b4a054037545389a368ce1d3190
Thomas Kaegi <tk@meeloo.ch>
2016-03-09 10:12:00 +0100

    Table-Prefix eingebaut

--- uninstall.sql
+++ uninstall.sql
@@ -1,2 +1,2 @@
-DROP TABLE IF EXISTS `rex_socialhub_entries`;
-DROP TABLE IF EXISTS `rex_socialhub_hashtags`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_entries`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_hashtags`;
