d39d6697ec82fe00c4323f62dfa77ffd64244b6c
Thomas Kaegi <tk@meeloo.ch>
2016-03-10 12:52:40 +0100

    Einrückung korrigiert; Prefix hinzugefügt

--- plugins/instagram/boot.php
+++ plugins/instagram/boot.php
@@ -1,12 +1,13 @@
 <?php
-
-$RSHI = socialhub_instagram::factory();
-
-if(rex_get('page') == 'socialhub/instagram/toggle_entry') {
-  $RSHI->toggleVisibility();
-  die();
-}
-if(rex_get('page') == 'socialhub/instagram/toggle_highlight') {
-  $RSHI->toggleHighlight();
-  die();
-}
+	$RSHI = rex_socialhub_instagram::factory();
+	
+	if(rex_get('page') == 'rex_socialhub/instagram/toggle_entry') {
+	  $RSHI->toggleVisibility();
+		die();
+	}
+	
+	if(rex_get('page') == 'rex_socialhub/instagram/toggle_highlight') {
+		$RSHI->toggleHighlight();
+		die();
+	}
+?>
