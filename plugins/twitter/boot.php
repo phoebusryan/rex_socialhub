5e4709153ee6ac1d376b77fed48a43ec105655cb
Thomas Kaegi <tk@meeloo.ch>
2016-03-10 10:32:38 +0100

    Sauber eingerückt

--- plugins/twitter/boot.php
+++ plugins/twitter/boot.php
@@ -1,12 +1,13 @@
 <?php
-
-$RSHT = socialhub_twitter::factory();
-
-if(rex_get('page') == 'socialhub/twitter/toggle_entry') {
-  $RSHT->toggleVisibility();
-  die();
-}
-if(rex_get('page') == 'socialhub/twitter/toggle_highlight') {
-  $RSHT->toggleHighlight();
-  die();
-}
+	$RSHT = rex_socialhub_twitter::factory();
+	
+	if(rex_get('page') == 'socialhub/twitter/toggle_entry') {
+		$RSHT->toggleVisibility();
+		die();
+	}
+	
+	if(rex_get('page') == 'socialhub/twitter/toggle_highlight') {
+		$RSHT->toggleHighlight();
+		die();
+	}
+?>
