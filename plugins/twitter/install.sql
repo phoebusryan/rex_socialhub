45f70059779fd57a78be894102e9756454691b9b
Thomas Kaegi <tk@meeloo.ch>
2016-03-11 11:58:23 +0100

    Tabelle umbenannt; Neue Tabelle hinzugefügt

--- plugins/twitter/install.sql
+++ plugins/twitter/install.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter` (
+CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_entries` (
  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
  `post_id` varchar(100) NOT NULL,
  `message` text NULL,
@@ -10,4 +10,13 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter` (
  `query` text Null,
  PRIMARY KEY (`id`),
  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_account` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `consumer_token` varchar(100) NOT NULL,
+ `consumer_secret_token` varchar(100) NOT NULL,
+ `access_token` varchar(100) NOT NULL,
+ `secret_token` varchar(100) NOT NULL,
+ PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
