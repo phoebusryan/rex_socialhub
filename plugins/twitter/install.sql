e0c9892ae21b679672adf46b7c7b8bb7bd4cb713
Thomas Kaegi <tk@meeloo.ch>
2016-03-14 16:33:16 +0100

    Querys abgeschlossen (Semikolon fehlte)

--- plugins/twitter/install.sql
+++ plugins/twitter/install.sql
@@ -27,11 +27,11 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_hashtag` (
  `hashtag` varchar(50) NOT NULL,
  `twitter_next_id` bigint(100) NOT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_timeline` (
  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
  `title` varchar(100) NOT NULL,
  `user_id` varchar(100) NOT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
