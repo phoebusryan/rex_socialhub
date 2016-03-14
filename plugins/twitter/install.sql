3a8f53640521ba808568df8a3082080237a6ec42
Thomas Kaegi <tk@meeloo.ch>
2016-03-14 16:28:16 +0100

    Angepasst an neue Tabellen

--- plugins/twitter/install.sql
+++ plugins/twitter/install.sql
@@ -21,3 +21,17 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_account` (
  `secret_token` varchar(100) NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_hashtag` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `hashtag` varchar(50) NOT NULL,
+ `twitter_next_id` bigint(100) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+
+CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_timeline` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `title` varchar(100) NOT NULL,
+ `user_id` varchar(100) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
--- plugins/twitter/uninstall.sql
+++ plugins/twitter/uninstall.sql
@@ -1,2 +1,4 @@
 DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_entry`;
 DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_account`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_hashtag`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_timeline`;
