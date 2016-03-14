0107da21c7b5f71bff91b5c1f0bcf674dda54290
Thomas Kaegi <tk@meeloo.ch>
2016-03-14 16:27:25 +0100

    Angepasst an neue Cronjobs; Einrückung korrigiert

--- plugins/twitter/install.php
+++ plugins/twitter/install.php
@@ -1,78 +1,57 @@
 <?php
-
-// ALTER TABLE `socialhub_hashtags` ADD `twitter_next_id` BIGINT(100) NOT NULL AFTER `hashtag`;
-$fields = [
-  'twitter_next_id'=>'BIGINT(100) NOT NULL AFTER `hashtag`',
-];
-  
-$cols = rex_sql::showColumns(rex::getTablePrefix().'socialhub_hashtags');
-foreach($fields as $fieldname => $fieldtype) {
-  $found = false;
-  foreach($cols as $field) {
-    if($field['name'] === $fieldname) {
-      $found = true;
-      break;
-    }
-  }
-
-  if(!$found) {
-    $sql = rex_sql::factory();
-    $sql->setQuery("ALTER TABLE `".rex::getTablePrefix().'socialhub_hashtags'."` ADD ".$fieldname." ".$fieldtype,array());
-  }
-}
-
-$newCron = rex_sql::factory();
-$newCron->setTable(REX_CRONJOB_TABLE);
-$newCron->setWhere(array('name'=>'Update Twitter'));
-$newCron->select();
-
-if($newCron->getRows() === 0) {
-  $newCron = rex_sql::factory();
-  $newCron->setTable(REX_CRONJOB_TABLE);
-  $newCron->setValue('name','Update Twitter');
-  $newCron->setValue('description','Prüft ob neue Twittereinträge auf den angegebenen Seiten vorhanden sind.');
-  $newCron->setValue('type','rex_cronjob_phpcallback');
-  $newCron->setValue('parameters','{"rex_cronjob_phpcallback_callback":"rex_socialhub_twitter::getEntriesByTimeline()"}');
-  $newCron->setValue('interval','|1|h|');
-  $newCron->setValue('nexttime','0000-00-00 00:00:00');
-  $newCron->setValue('environment','|0|1|');
-  $newCron->setValue('execution_moment',0);
-  $newCron->setValue('status',0);
-  $newCron->setValue('execution_start',date('Y-m-d').' 01:00:00');
-
-  $newCron->addGlobalUpdateFields();
-  $newCron->addGlobalCreateFields();
-  try {
-    $newCron->insert();
-  } catch (rex_sql_exception $e) {
-    echo rex_view::warning($e->getMessage());
-  }
-}
-
-$newCron->reset();
-$newCron->setTable(REX_CRONJOB_TABLE);
-$newCron->setWhere(array('name'=>'Update Twitter-Hashtags'));
-$newCron->select();
-
-if($newCron->getRows() === 0) {
-  $newCron = rex_sql::factory();
-  $newCron->setTable(REX_CRONJOB_TABLE);
-  $newCron->setValue('name','Update Twitter-Hashtags');
-  $newCron->setValue('description','Prüft ob neue Hashtags auf Twitter vorhanden sind.');
-  $newCron->setValue('type','rex_cronjob_phpcallback');
-  $newCron->setValue('parameters','{"rex_cronjob_phpcallback_callback":"rex_socialhub_twitter::getEntriesByHashtag()"}');
-  $newCron->setValue('interval','|1|h|');
-  $newCron->setValue('nexttime','0000-00-00 00:00:00');
-  $newCron->setValue('environment','|0|1|');
-  $newCron->setValue('execution_moment',0);
-  $newCron->setValue('status',1);
-  $newCron->setValue('execution_start',date('Y-m-d').' 01:00:00');
-
-  $newCron->addGlobalUpdateFields();
-  $newCron->addGlobalCreateFields();
-  try {
-    $newCron->insert();
-  } catch (rex_sql_exception $e) {
-    echo rex_view::warning($e->getMessage());
-  }
-}
+	$newCron = rex_sql::factory();
+	$newCron->setTable(REX_CRONJOB_TABLE);
+	$newCron->setWhere(array('name'=>'Update Twitter-Timelines'));
+	$newCron->select();
+	
+	if($newCron->getRows() === 0) {
+		$newCron = rex_sql::factory();
+		$newCron->setTable(REX_CRONJOB_TABLE);
+		$newCron->setValue('name','Update Twitter-Timelines');
+		$newCron->setValue('description','Prüft ob neue Twittereinträge auf den angegebenen Seiten vorhanden sind.');
+		$newCron->setValue('type','rex_cronjob_phpcallback');
+		$newCron->setValue('parameters','{"rex_cronjob_phpcallback_callback":"rex_socialhub_twitter::getEntriesByTimeline()"}');
+		$newCron->setValue('interval','|1|h|');
+		$newCron->setValue('nexttime','0000-00-00 00:00:00');
+		$newCron->setValue('environment','|0|1|');
+		$newCron->setValue('execution_moment',0);
+		$newCron->setValue('status',0);
+		$newCron->setValue('execution_start',date('Y-m-d').' 01:00:00');
+		
+		$newCron->addGlobalUpdateFields();
+		$newCron->addGlobalCreateFields();
+		try {
+			$newCron->insert();
+		} catch (rex_sql_exception $e) {
+			echo rex_view::warning($e->getMessage());
+		}
+	}
+	
+	$newCron->reset();
+	$newCron->setTable(REX_CRONJOB_TABLE);
+	$newCron->setWhere(array('name'=>'Update Twitter-Hashtags'));
+	$newCron->select();
+	
+	if($newCron->getRows() === 0) {
+		$newCron = rex_sql::factory();
+		$newCron->setTable(REX_CRONJOB_TABLE);
+		$newCron->setValue('name','Update Twitter-Hashtags');
+		$newCron->setValue('description','Prüft ob neue Hashtags auf Twitter vorhanden sind.');
+		$newCron->setValue('type','rex_cronjob_phpcallback');
+		$newCron->setValue('parameters','{"rex_cronjob_phpcallback_callback":"rex_socialhub_twitter::getEntriesByHashtag()"}');
+		$newCron->setValue('interval','|1|h|');
+		$newCron->setValue('nexttime','0000-00-00 00:00:00');
+		$newCron->setValue('environment','|0|1|');
+		$newCron->setValue('execution_moment',0);
+		$newCron->setValue('status',1);
+		$newCron->setValue('execution_start',date('Y-m-d').' 01:00:00');
+		
+		$newCron->addGlobalUpdateFields();
+		$newCron->addGlobalCreateFields();
+		try {
+		  $newCron->insert();
+		} catch (rex_sql_exception $e) {
+		  echo rex_view::warning($e->getMessage());
+		}
+	}
+?>
--- plugins/twitter/uninstall.php
+++ plugins/twitter/uninstall.php
@@ -1,22 +1,21 @@
 <?php
-
-
-$newCron = rex_sql::factory();
-$newCron->setTable(REX_CRONJOB_TABLE);
-$newCron->setWhere(array('name'=>'Update Twitter'));
-
-try {
-  $newCron->delete();
-} catch (rex_sql_exception $e) {
-  echo rex_view::warning($e->getMessage());
-}
-
-$newCron = rex_sql::factory();
-$newCron->setTable(REX_CRONJOB_TABLE);
-$newCron->setWhere(array('name'=>'Update Twitter-Hashtags'));
-
-try {
-  $newCron->delete();
-} catch (rex_sql_exception $e) {
-  echo rex_view::warning($e->getMessage());
-}
+	$newCron = rex_sql::factory();
+	$newCron->setTable(REX_CRONJOB_TABLE);
+	$newCron->setWhere(array('name'=>'Update Twitter-Timelines'));
+	
+	try {
+		$newCron->delete();
+	} catch (rex_sql_exception $e) {
+		echo rex_view::warning($e->getMessage());
+	}
+	
+	$newCron = rex_sql::factory();
+	$newCron->setTable(REX_CRONJOB_TABLE);
+	$newCron->setWhere(array('name'=>'Update Twitter-Hashtags'));
+	
+	try {
+		$newCron->delete();
+	} catch (rex_sql_exception $e) {
+		echo rex_view::warning($e->getMessage());
+	}
+?>
