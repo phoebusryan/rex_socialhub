3ca343b460534efb2b1fdfb992f35ad9737028c2
Thomas Kaegi <tk@meeloo.ch>
2016-03-15 15:29:17 +0100

    Tabelle entfernt -> in Addon verschoben

--- plugins/twitter/install.sql
+++ plugins/twitter/install.sql
@@ -1,17 +1,3 @@
-CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_entry` (
- `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
- `post_id` varchar(100) NOT NULL,
- `message` text NULL,
- `image` text NULL,
- `name` varchar(100) NOT NULL,
- `author` text NULL,
- `visible` char(1) NOT NULL DEFAULT '1',
- `highlight` char(1) NOT NULL DEFAULT '0',
- `query` text Null,
- PRIMARY KEY (`id`),
- KEY `post_id` (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
 CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%socialhub_twitter_account` (
  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
  `title` varchar(100) NOT NULL,
--- plugins/twitter/uninstall.sql
+++ plugins/twitter/uninstall.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_entry`;
 DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_account`;
 DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_hashtag`;
 DROP TABLE IF EXISTS `%TABLE_PREFIX%socialhub_twitter_timeline`;
