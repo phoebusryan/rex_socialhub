3ff08ce7c69ee64e56ba8a19ef171e9cc145ad2b
Thomas Kaegi <tk@meeloo.ch>
2016-03-16 14:54:37 +0100

    Plugin komplett entfernt

--- plugins/share/fragments/codes/share_vars.php
+++
@@ -1,4 +0,0 @@
-<?php
-  socialhub_share::setConfig('share_url','eine URL');
-  socialhub_share::setConfig('share_title','Ein Titel');
-?>
--- plugins/share/help.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$fragment = new rex_fragment();
-$content = $fragment->parse('help.php');
-
-echo $content;
